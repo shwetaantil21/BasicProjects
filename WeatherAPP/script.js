f44855525d54b8cd13058fa83b23bf7d2ce65488
shwetaantil21 <shwetaantil590@gmail.com>
2024-04-18 13:39:51 +0530

    Add new files

--- WeatherAPP/script.js
+++ WeatherAPP/script.js
@@ -21,7 +21,7 @@ async function checkWeather(city) {
         document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
         document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
-        // document.querySelector('.weather').innerHTML = data.weather[0].main;
+
 
         if (data.weather[0].main === "Clouds") {
             weatherIcon.src = 'images/clouds.png';
@@ -42,32 +42,6 @@ async function checkWeather(city) {
         document.querySelector('.weather').style.display = "block";
         document.querySelector('.error').style.display = "none";
     }
-    // var data = await response.json();
-    // console.log(data);
-
-    // document.querySelector('.city').innerHTML = data.name;
-    // document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + "°C";
-    // document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-    // document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
-    // document.querySelector('.weather').innerHTML = data.weather[0].main;
-
-    // if (data.weather[0].main === "Clouds") {
-    //     weatherIcon.src = 'images/clouds.png';
-    // }
-    // else if (data.weather[0].main === "Clear") {
-    //     weatherIcon.src = 'images/clear.png';
-    // }
-    // else if (data.weather[0].main === "Rain") {
-    //     weatherIcon.src = 'images/rain.png';
-    // }
-    // else if (data.weather[0].main === "Drizzle") {
-    //     weatherIcon.src = 'images/drizzle.png';
-    // }
-    // else if (data.weather[0].main === "Mist") {
-    //     weatherIcon.src = 'images/mist.png';
-    // }
-
-    // document.querySelector('.weather').style.display = "block";
 }
 
 
